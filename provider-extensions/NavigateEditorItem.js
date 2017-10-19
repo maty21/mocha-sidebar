eb7806458818d81928d113bd0d1f7a62345c33c7
maty zisserman <maty21@gmail.com>
2017-10-19 10:44:54 -1000

    fix line reocgintion for windows

--- provider-extensions/NavigateEditorItem.js
+++ provider-extensions/NavigateEditorItem.js
@@ -1,19 +1,40 @@
 const vscode = require('vscode');
 const fs = require('fs');
-const lineNumber = require('line-number');
+//const lineNumber = require('line-number');
+const splitLines = require('split-lines');
 const navigateEditorItem = (file, name) => {
     // let re = new RegExp(`/${name}+/g`);
     let re = new RegExp(`${name}+`);
 
     let fixture = fs.readFileSync(file, 'utf8');
-    let line = lineNumber(fixture, re);
-   // console.log(line);
+    let line = _lineNo(fixture, re);
     if (line.length == 0) {
         line.push({ line: "", number: 0, match: "" })
     }
+    let fixedLine = line.filter(l => {
+        let t = l.line.split('(')[1].split(',')[0].replace(/'/gi, "").replace(/"/gi, "").replace(/`/gi, "");
+        return t == name;
+    })
+    // console.log(line);
+    if (fixedLine.length == 0) {
+        fixedLine.push(line[0])
+    }
 
-    return line;
-   
+    return fixedLine;
+
 }
+
+
+let _lineNo = (str, re) => {
+    return splitLines(str).map((line, i) => {
+        if (re.test(line)) {
+            return {
+                line: line,
+                number: i + 1,
+                match: line.match(re)[0]
+            };
+        }
+    }).filter(Boolean);
+};
 
 module.exports = navigateEditorItem;
