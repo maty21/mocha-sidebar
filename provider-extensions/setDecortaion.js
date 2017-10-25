9584801429fa6f8f56051eefe4b480ce5e67b72d
Cube <maty21@gmail.com>
2017-10-25 07:27:43 -1000

    Update setDecortaion.js
    add decorators code

--- provider-extensions/setDecortaion.js
+++ provider-extensions/setDecortaion.js
@@ -1,9 +1,45 @@
 const vscode = require('vscode');
-const setDecoration = (resStauts,test)=>{
-   
-    vscode.editor.setDecorations()
+const decorationType = require('./decorationType');
+const consts = require('./consts');
+const setDecoration = (resStatus, test) => {
+
+    let style = getStyle(resStatus);
+    let decorators = {
+        range: new vscode.Range(test.line[0].number - 1, 0, test.line[0].number - 1, 0),
+        hoverMessage: resStatus,
+        //range: new vscode.Range(line[0].number - 1, it.start.column - 1,  line[0].number - 1, it.start.column + 1)
+    }
+    try {
+        vscode.window.activeTextEditor.setDecorations(style, [decorators])
+    } catch (error) {
+        console.log(error);
+    }
 
 
+    // return blocks.map(it => {
+    //     return {
+    //       // VS Code is indexed starting at 0
+    //       // jest-editor-support is indexed starting at 1
+    //       range: new vscode.Range(it.start.line - 1, it.start.column - 1, it.start.line - 1, it.start.column + 1),
+    //       hoverMessage: nameForState(it.name, state),
+    //     }
+    //   })
 
-    
+
 }
+
+const getStyle = (status) => {
+    let decorationStyle = null;
+    switch (status) {
+        case consts.PASSED:
+            decorationStyle = decorationType.pass()
+            break;
+        case consts.FAILED:
+            decorationStyle = decorationType.fail()
+            break;
+        default:
+            decorationStyle = decorationType.notRun()
+    }
+    return decorationStyle;
+}
+module.exports = setDecoration;
