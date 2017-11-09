8878ae4a6067afd920e5170b170dbf2aa7c62095
yehiyam <yehiyam@gmail.com>
2017-11-09 22:52:57 +0200

    add test case for duplicate lens

---
+++ test/workspace/test/duplicateLens.js
@@ -0,0 +1,17 @@
+describe('level1', () => {
+    describe('level2_1', () => {
+        it('same name', () => {
+            
+        });    
+    });
+    describe('level2_2', () => {
+        it('same name', () => {
+            
+        });    
+    });
+    describe('level2_3', () => {
+        it('same name', () => {
+            
+        });    
+    });
+});
