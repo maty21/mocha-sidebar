3a5ef0912dbdc92176e8a99239cadafc399c975e
Kendall Frey <kendfrey@gmail.com>
2017-12-19 17:17:07 -0500

    better handling of special characters in test names

--- provider-extensions/NavigateEditorItem.js
+++ provider-extensions/NavigateEditorItem.js
@@ -6,7 +6,7 @@ const navigateEditorItem = (file, name) => {
     let fixture = fs.readFileSync(file, 'utf8');
     let line = _lineNo(fixture, name);
     if (line.length == 0) {
-        line.push({ line: "", number: 0, match: "" })
+        line.push({ line: "", number: 1, match: "" })
     }
     let fixedLine = line.filter(l => {
         try {
@@ -29,12 +29,17 @@ const navigateEditorItem = (file, name) => {
 
 
 let _lineNo = (str, name) => {
+    const re = new RegExp(
+        JSON.stringify(name) // Handle things like \\ and \t
+        .replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&") // Escape any regex metacharacters
+        .replace(/^"(.*)"$/, "([\"'`])$1\\1") // Only count matches surrounded by " ' or `
+    );
     return splitLines(str).map((line, i) => {
-        if (line.includes(name)) {
+        if (re.test(line)) {
             return {
                 line: line,
                 number: i + 1,
-                match: name
+                match: line.match(re)[0]
             };
         }
     }).filter(Boolean);
