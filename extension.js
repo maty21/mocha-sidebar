38374d73b3b10721078c53f7175e15a822e97002
Maty Zisserman <maty21@Matys-MacBook-Pro.local>
2019-04-30 13:51:22 +0300

    refactoring

--- extension.js
+++ extension.js
@@ -12,15 +12,20 @@ const decorationProvider = require("./lib/provider-extensions/setDecortaion");
 const subscriptionRegister = require("./lib/subscriptionRegister");
 const coverage = require("./lib/coverage/code-coverage");
 const core = require("./lib/core");
+const testStatusBar = require("./lib/provider-extensions/testStatusBar");
 
-function activate(context) {
+function bootstrap(context) {
   const _treeProvider = new treeProvider();
-  const _decorationProvider = new decorationProvider();
   const _codeLensProvider = new lensProvider(context, core);
-  const subscriptions = context.subscriptions;
-  subscriptionRegister(context, _treeProvider);
+  const _testStatusBar = new testStatusBar();
+  const _decorationProvider = new decorationProvider();
+  const _subscriptionRegister = new subscriptionRegister();
 
   core.run().then(r => {
+    _decorationProvider.init();
+    _testStatusBar.init();
+    _subscriptionRegister.init(context, _treeProvider);
+
     vscode.window.registerTreeDataProvider("mocha", _treeProvider);
     vscode.languages.registerCodeLensProvider(_codeLensProvider.selector, _codeLensProvider);
   });
@@ -31,7 +36,7 @@ function activate(context) {
   }
 }
 
-exports.activate = activate;
+exports.activate = bootstrap;
 
 // this method is called when your extension is deactivated
 function deactivate() {}
