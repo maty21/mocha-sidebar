4cf7b7871a290c42317add0873b6ad3b039ce0d7
Maty Zisserman <maty21@Matys-MacBook-Pro.local>
2019-05-20 14:56:08 +0300

    fix describer running for lens and tree + fix meta from array to object

--- test/workspace/test/environment.js
+++ test/workspace/test/environment.js
@@ -6,13 +6,6 @@ const expect = require("chai").expect;
 let isFirst = true;
 
 describe("green 4", function() {
-  // beforeEach(() => {
-  //   if (isFirst) {
-  //     // this assertion fails, but mocha-sidebar test entry does not turn red
-  //     isFirst = false;
-  //     assert.equal(1, 2);
-  //   }
-  // });
   it("inner green 4 test", function(done) {
     setTimeout(() => {
       expect(1).to.equal(1);
@@ -33,31 +26,41 @@ describe("test", function() {
   });
 });
 
-// describe('green 5', () => {
-//   describe('describe level 1', () => {
-//     describe('describe level 2', () => {
-//       it('inner green 5 level 2', function () {
-//         assert(process.env.HOME || process.env.PATHEXT);
-//       });
-//       it(' inner green second 5 level 2', function () {
-//         assert(process.env.HOME || process.env.PATHEXT);
-//       });
-//     });
-//     it('inner green 4', function () {
-//       assert(process.env.HOME || process.env.PATHEXT);
-//     })
+describe("outer", () => {
+  describe("inner", () => {
+    it("inner-test", () => {
+      expect(1).to.be.equal(1);
+    });
+  });
+  it("outer-test", () => {
+    expect(1).to.be.equal(1);
+  });
+});
+describe("green 5", () => {
+  describe("describe level 1", () => {
+    describe("describe level 2", () => {
+      it("inner green 5 level 2", function() {
+        assert(process.env.HOME || process.env.PATHEXT);
+      });
+      it(" inner green second 5 level 2", function() {
+        assert(process.env.HOME || process.env.PATHEXT);
+      });
+    });
+    it("inner green 4", function() {
+      assert(process.env.HOME || process.env.PATHEXT);
+    });
 
-//     it('inner green 5 level  1', function () {
-//       assert(process.env.HOME || process.env.PATHEXT);
-//     });
-//     it('inner green second 5 level 1 ', function () {
-//       assert(process.env.HOME || process.env.PATHEXT);
-//     });
-//   });
-//   it('inner green 5 level 0', function () {
-//     assert(process.env.HOME || process.env.PATHEXT);
-//   });
-//   it('long timeout', function (done) {
-//     setTimeout(done,10000);
-//   });
-// });
+    it("inner green 5 level  1", function() {
+      assert(process.env.HOME || process.env.PATHEXT);
+    });
+    it("inner green second 5 level 1 ", function() {
+      assert(process.env.HOME || process.env.PATHEXT);
+    });
+  });
+  it("inner green 5 level 0", function() {
+    assert(process.env.HOME || process.env.PATHEXT);
+  });
+  it("long timeout", function(done) {
+    setTimeout(done, 10000);
+  });
+});
